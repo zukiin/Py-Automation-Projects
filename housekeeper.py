c6fdd3e4f868e0efc508537a53fb0b7ac61fc0c5
zukinkalitshana <zukinkalitshana@icloud.com>
2024-08-01 21:05:41 +0200

    Encapsulate code

--- housekeeper.py
+++ housekeeper.py
@@ -10,36 +10,41 @@ folders = {
     "Misc": []
 }
 
-def organize_and_move():
-    for file in os.listdir(downloads_path):
-        file_path = os.path.join(downloads_path, file)
-        
-        if os.path.isfile(file_path):
-            file_ext = os.path.splitext(file)[1].lower()
-            moved = False
+class HouseKeeper:
+    def __init__(self):
+        pass
 
-            for folder, extensions in folders.items():
-                if file_ext in extensions:
-                    folder_path = os.path.join(downloads_path, folder)
-                    
-                    # Create folder if it doesn't exist
+    def organize_and_move(self):
+        for file in os.listdir(downloads_path):
+            file_path = os.path.join(downloads_path, file)
+            
+            if os.path.isfile(file_path):
+                file_ext = os.path.splitext(file)[1].lower()
+                moved = False
+
+                for folder, extensions in folders.items():
+                    if file_ext in extensions:
+                        folder_path = os.path.join(downloads_path, folder)
+                        
+                        # Create folder if it doesn't exist
+                        if not os.path.exists(folder_path):
+                            os.makedirs(folder_path)
+                        
+                        print(f"Moving '{file}' to '{folder_path}'...")
+                        shutil.move(file_path, os.path.join(folder_path, file))
+                        moved = True
+                        break
+
+                # Move to 'Misc' if no matching folder
+                if not moved:
+                    print(f"Moving '{file}' to Misc folder...")
+                    folder_path = os.path.join(downloads_path, "Others")
                     if not os.path.exists(folder_path):
                         os.makedirs(folder_path)
-                    
-                    print(f"Moving '{file}' to '{folder_path}'...")
                     shutil.move(file_path, os.path.join(folder_path, file))
-                    moved = True
-                    break
 
-            # Move to 'Misc' if no matching folder
-            if not moved:
-                print(f"Moving '{file}' to Misc folder...")
-                folder_path = os.path.join(downloads_path, "Others")
-                if not os.path.exists(folder_path):
-                    os.makedirs(folder_path)
-                shutil.move(file_path, os.path.join(folder_path, file))
+    def auto_arrange(self):
+        pass
 
-def auto_arrange():
-    pass
-
-organize_and_move()
+housekeeper = HouseKeeper()
+housekeeper.organize_and_move()
